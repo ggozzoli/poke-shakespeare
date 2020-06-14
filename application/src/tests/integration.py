2a2cb6e39f44b57174935b32e105c7c2bba9d02b
Gabriele Gozzoli <g.gozzoli@gmail.com>
2020-06-14 16:06:09 +0200

    Integration tests structure

---
+++ application/src/tests/integration.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+
+import requests
+
+
+class IntegrationTestCase(TestCase):
+    base_url = 'http://localhost:5000/pokemon/{pokemonName}'
+
+    def test_charizard_description(self):
+        url = self.base_url.format(pokemonName='charizard')
+        r = requests.get(url=url)
+        self.assertEqual(200, r.status_code)
+        self.assertEqual('charizard', r.json().get('name'))
+
+    def test_not_found(self):
+        r = requests.get(url=self.base_url)
+        self.assertEqual(404, r.status_code)
+
+    def test_method_not_allowed(self):
+        url = self.base_url.format(pokemonName='charizard')
+        r = requests.post(url=url)
+        self.assertEqual(405, r.status_code)
